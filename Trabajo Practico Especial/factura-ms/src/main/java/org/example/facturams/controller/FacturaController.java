a34ce41afdaac1ab1008c919060ce03715479a13
ValentinBulnes <bulnesvalentin@gmail.com>
2025-11-08 13:39:30 -0300

    gateway

--- Trabajo Practico Especial/factura-ms/src/main/java/org/example/facturams/controller/FacturaController.java
+++ Trabajo Practico Especial/factura-ms/src/main/java/org/example/facturams/controller/FacturaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Facturas")
+@RequestMapping("/facturas")
 public class FacturaController {
 
     private final FacturaService facturaService;

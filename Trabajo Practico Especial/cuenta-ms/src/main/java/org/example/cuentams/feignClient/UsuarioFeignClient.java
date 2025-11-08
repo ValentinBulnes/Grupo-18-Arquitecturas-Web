b22d4ba78200fb04c02d2fc250c8e3b9dee3f537
ValentinBulnes <bulnesvalentin@gmail.com>
2025-11-08 17:34:46 -0300

    Actualizo versiones de Spring Boot y Spring Cloud

--- Trabajo Practico Especial/cuenta-ms/src/main/java/org/example/cuentams/feignClient/UsuarioFeignClient.java
+++ Trabajo Practico Especial/cuenta-ms/src/main/java/org/example/cuentams/feignClient/UsuarioFeignClient.java
@@ -5,13 +5,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "usuario-ms", url = "http://localhost:8001")
-@RequestMapping("/usuarios")
+// @RequestMapping("/usuarios")
     public interface UsuarioFeignClient {
 
 //    @PostMapping
 //    Cuenta save(@RequestBody Cuenta cuenta);
 
-    @GetMapping("/{id}")
+    @GetMapping("/usuarios/{id}")
     UsuarioDTO obtenerUsuario(@PathVariable Long id);
 
 

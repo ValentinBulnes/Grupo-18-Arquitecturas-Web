f11a049ab2b6b8efb9542d1d504f6c5f78465a69
Luca Gorosito <lucagoro04@gmail.com>
2025-11-10 21:57:53 -0300

    --

--- Trabajo Practico Especial/usuario-ms/src/main/java/org/example/usuarioms/usuario/repository/UsuarioRepository.java
+++ Trabajo Practico Especial/usuario-ms/src/main/java/org/example/usuarioms/usuario/repository/UsuarioRepository.java
@@ -4,22 +4,8 @@ import org.example.usuarioms.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    // CRUD (ver si va)
-    // Insertar
-    Usuario save(Usuario usuario);
-
-    // Obtener por id
-    Optional<Usuario> findById(Long id);
-
-    // Obtener todos
-    List<Usuario> findAll();
-
-    // Eliminar usuario por id
-    void deleteById(Long id);
 }

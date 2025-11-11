3ee05f4c8b602ab0b6be7535c22e93c93f4d8585
ValentinBulnes <bulnesvalentin@gmail.com>
2025-11-11 20:49:53 -0300

    actualizo url del CuentaFeignClient a puerto 8001

--- Trabajo Practico Especial/viaje-ms/src/main/java/com/example/viajems/feignClients/CuentaFeignClient.java
+++ Trabajo Practico Especial/viaje-ms/src/main/java/com/example/viajems/feignClients/CuentaFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.viajems.DTO.CuentaDTO;
 
-@FeignClient(name = "cuenta-ms", url = "http://localhost:8002")
+@FeignClient(name = "cuenta-ms", url = "http://localhost:8001")
 public interface CuentaFeignClient {
 
     @GetMapping("/cuentas/{id}")

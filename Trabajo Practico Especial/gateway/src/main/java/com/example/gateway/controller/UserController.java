57b2eea75f781731744311897d217b8c4c02a5b2
ValentinBulnes <bulnesvalentin@gmail.com>
2025-11-23 17:15:52 -0300

    archivos jwt en gateway

---
+++ Trabajo Practico Especial/gateway/src/main/java/com/example/gateway/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.gateway.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody UserDTO userDTO) {
+
+        // Guarda el usuario en la base del gateway (modelo profes)
+        Long id = userService.saveUser(userDTO);
+
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
+    }
+}

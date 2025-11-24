e7e38ed1f326d6d28b521366d8e7026307c0b371
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2025-11-24 12:26:16 -0300

    Agregado puerto chat-ms

--- Trabajo Practico Especial/chat-ms/src/main/java/com/example/chatms/ChatMsApplication.java
+++ Trabajo Practico Especial/chat-ms/src/main/java/com/example/chatms/ChatMsApplication.java
@@ -2,8 +2,19 @@ package com.example.chatms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 public class ChatMsApplication {
 
     public static void main(String[] args) {

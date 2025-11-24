7b9a24b6d66d1f543faa9b1a16ac5c5edc82e58f
JIBertoldi <jbertoldi@alumnos.exa.unicen.edu.ar>
2025-11-24 11:51:42 -0300

    Agregado de servicio chat con ia

---
+++ Trabajo Practico Especial/chat-ms/src/main/java/com/example/chatms/dto/RespuestaApi.java
@@ -0,0 +1,13 @@
+package com.example.chatms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RespuestaApi<T> {
+    private boolean ok;
+    private String mensaje;
+    private T datos;
+}
+
